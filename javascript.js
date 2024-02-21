e8124f976698008e05423828c54e10a189db1df6
SgtKazoo <108840734+SgtKazoo@users.noreply.github.com>
2024-02-21 06:07:09 -0600

    Made the turn manager so it plays 5 rounds, keeps score, and displays the result at the end

--- javascript.js
+++ javascript.js
@@ -14,38 +14,67 @@ function getComputerChoice(){
 
 function playRound(playerSelection, computerSelection){
     playerSelection = playerSelection.toLowerCase();
-    let result = "No result given";
+    let result = null;
     if (playerSelection == "rock"){
         if (computerSelection == "rock"){
-            result = "Tie! Rock ties with rock."
+            console.log("Tie! Rock ties with rock.")
+            result = 2;
         } else if (computerSelection == "paper"){
-            result = "You lose! Rock loses to paper"
+            console.log("You lose! Rock loses to paper")
+            result = 0;
         } else{
-            result = "You win! Rock beats scissors"
+            console.log("You win! Rock beats scissors")
+            result = 1;
         }
     } else if (playerSelection == "paper"){
         if (computerSelection == "rock"){
-            result = "You win! Paper beats rock."
+            console.log("You win! Paper beats rock.")
+            result = 1;
         } else if (computerSelection == "paper"){
-            result = "Tie! Paper ties with paper"
+            console.log("Tie! Paper ties with paper")
+            result = 2;
         } else{
-            result = "You lose! Paper loses to scissors"
+            console.log("You lose! Paper loses to scissors")
+            result = 0;
         }
     //playerSelection is paper
     } else{
         if (computerSelection == "rock"){
-            result = "You lose! Scissors loses to rock"
+            console.log("You lose! Scissors loses to rock")
+            result = 0;
         } else if (computerSelection == "paper"){
-            result = "You win! Scissors beats paper"
+            console.log("You win! Scissors beats paper")
+            result = 1;
         } else{
-            result = "Tie! Scissors ties with scissors"
+            console.log("Tie! Scissors ties with scissors")
+            result = 2;
         }
     }
     return result;
 }
 
-const playerSelection = "rock";
-const computerSelection = getComputerChoice();
-console.log(playRound(playerSelection, computerSelection));
+function playGame(){
+    let wins = 0;
+    let losses = 0;
+    console.log("Lets play a game of rock paper scissors. Best out of 5 wins!");
+    for (let i = 0; i < 5; i++){
+        let playerChoice = prompt("Rock paper scissors... SHOOT!");
+        let result = playRound(playerChoice, getComputerChoice())
+        if (result == 0){
+            losses++;
+        } else if (result == 1){
+            wins++;
+        } else{
+            //tie so do nothing
+        }
+    }
+    if (wins > losses){
+        console.log(wins + " total wins and " +losses+ " total losses. You win!")
+    } else if (wins < losses){
+        console.log(wins + " total wins and " +losses+ " total losses. You lose!")
+    } else {
+        console.log(wins + " total wins and " +losses+ " total losses. Tie!")
+    }
+}
 
-
+playGame();
